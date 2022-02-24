d523d6d05c8e27191592afb2bf07235dd8c11fa8
ArturDokhno <artur.dohno@icloud.com>
2022-02-24 15:33:50 +0500

    Initial Commit

---
+++ ToDoListCoreData2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListCoreData2
+//
+//  Created by Артур Дохно on 24.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
